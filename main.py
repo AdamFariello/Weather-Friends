6376f9ae6c9c20a10a073b91e8003a76305171cc
Adam Fariello <ajf270@scarletmail.rutgers.edu>
2025-05-13 00:19:04 -0400

    Added getting city function

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import requests
+import pprint
 mainUrl = "https://geocoding-api.open-meteo.com/v1/"
+paramater = "search?"
 #search?name=Berlin&count=10&language=en&format=json"
 
-def getPage():
-    res = requests.get(mainUrl).json()
-    print(res)
+# Args:
+# 1) name	
+# 2) count
+# 3) format	
+# 4) language	 -- Default
+# 5) apikey	
+# 6) countryCode
+
+def getPage(city):
+    apiCall = mainUrl
+    def addCity(city):
+        return apiCall + paramater + "name=" + city + "&" 
+
+    apiCall = addCity(city)
+    res = requests.get(apiCall).json()
+    pprint.pprint(res)
 
 
 def main():
-    getPage()
+    getPage("Berlin")
 
 if __name__ == '__main__': main()
